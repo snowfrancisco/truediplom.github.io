d67bf11589b74fca3105c04a5fee9770bfb5b650
Snowfrancisco <38356693+snowfrancisco@users.noreply.github.com>
2018-06-01 19:34:27 +0400

    aasd
    asd

---
+++ php/send.php
@@ -0,0 +1,78 @@
+<?php
+
+$adminemail = "taske.hlP@gmail.com";
+
+$date=date("d.m.y");
+
+$time=date("H:i");
+
+$backurl="http://mickel"
+
+
+$name=$_POST['usrname'];
+$email=$_POST['email'];
+$msg=$_POST['comment'];
+
+//________________________________________________
+
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$msg=" 
+ 
+ 
+<p>Имя: $name</p> 
+ 
+ 
+<p>E-mail: $email</p> 
+ 
+ 
+<p>Сообщение: $msg</p> 
+ 
+ 
+";
+
+//________________________________________________
+
+mail("$adminemail", "$date $time Сообщение от $name", "$msg");
+
+//___________БД__________
+
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+ 
+fwrite($f,"\n $msg "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+
+//_______________________________________________
+
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$msg 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+
+?>
